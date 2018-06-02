5124c17e0e36cc56e0e6950a634fe42fb4c176bd
Puneet <codydeny@gmail.com>
2018-06-01 18:33:21 -0700

    Website Template

---
+++ send.php
@@ -0,0 +1,22 @@
+    <?php
+        // Account details
+        $apiKey = urlencode('VEhoDuOrivA-ybhtRElTTCoNlXIOkqLwUJIg3zhXBz');
+        
+        // Message details
+        $numbers = urlencode('919997066760');
+        $sender = urlencode('TXTLCL');
+        $Msgdata = "Name: ".$_POST['name']." \nMobile Number: ".$_POST['mob']."\nDate: ".$_POST['date']."\nNo. of Rooms: ".$_POST['nrooms']. "\nType of Room: ".$_POST['roomtype'];
+        $message = rawurlencode($Msgdata);
+     
+        // Prepare data for POST request
+        $data = 'apikey=' . $apiKey . '&numbers=' . $numbers . "&sender=" . $sender . "&message=" . $message;
+     
+        // Send the GET request with cURL
+        $ch = curl_init('https://api.textlocal.in/send/?' . $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        // Process your response here
+        echo $response;
+    ?>
